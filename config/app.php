e9d372cf10c5cd6bec62a34a361ddc8fb6fba931
tstuttard <tom.e.stuttard@gmail.com>
2016-04-27 12:28:18 +0100

    Install tactician and ide helper
    ignore  _ide_helper.php

--- config/app.php
+++ config/app.php
@@ -147,6 +147,8 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+        Madewithlove\Tactician\ServiceProvider::class,
 
         /*
          * Application Service Providers...
